12b16cd695d6951e9b4147097acc842e717e5a07
famsameerkhan1342 <famsameerkhan1342@gmail.com>
2022-12-12 14:56:41 +0530

    add introducation

---
+++ Introduction.js
@@ -0,0 +1,60 @@
+/*
+what is a programming language ?
+
+   pro'gramming language software develop karna ka leya use hoti hai .
+   programming language internly level jo bhi kam hai vo karti hai .
+
+
+what is high level programming language ?
+
+    high level programming language ka matlab hai ki code likhna ka 
+    alava bhut sara asya action hotay hai jo language khud handel karti hai .
+       for example :
+                 1) memory managemant.
+                 2) communication with heardware.
+       
+  
+what is front-end ?
+
+    front-end kisi bhi website application ka vha part hota hai jisa hum 
+    intract kar sakta hai.
+
+what is back-end ?
+
+    back-end software ka vha part hota jo bhind the seen hota work kar ta hai.
+        for example :
+                     1) data save.
+                     2)data trasfar.
+                     3)data update.
+
+
+  Introduction of HTML 
+    
+        HTML stand for Hyper Text Markup Language.jo ki web page bananay ka 
+        leya use hoti hai. web page ak html file hoti hai. jo kisi bhi web site ma
+        ak page ko repersant karti hai.
+        
+   Introduction of CSS 
+   
+         CSS stand for cascading style sheets.css html page ma styleing karna
+          ka leya use hoti hai.       
+
+   
+ Introduction of JavaScript
+
+          javascript ak high level programming language hai. jisa hum web ma 
+          use karta hai. javascript ki help se hum website ko intractive bana
+          sakta hai.phala javascript only web ma use hoti thi lakin ab hum 
+          javascript ko beck-end ma bhi use kar sakta hai. javascript ko
+         back-end ma use karna ka leya node.js use ma le jati hai.
+  
+         
+ History of javascript 
+         Brendan Eich in 1995        
+
+
+
+
+
+                     
+*/
