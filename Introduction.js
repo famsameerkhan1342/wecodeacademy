da41b98e9e5036c6193d4955c64fb128f7d2e82c
famsameerkhan1342 <famsameerkhan1342@gmail.com>
2022-12-13 12:09:05 +0530

    modified files

--- Introduction.js
+++ Introduction.js
@@ -28,19 +28,19 @@ what is back-end ?
                      3)data update.
 
 
-  Introduction of HTML 
+Introduction of HTML 
     
         HTML stand for Hyper Text Markup Language.jo ki web page bananay ka 
         leya use hoti hai. web page ak html file hoti hai. jo kisi bhi web site ma
         ak page ko repersant karti hai.
         
-   Introduction of CSS 
+Introduction of CSS 
    
          CSS stand for cascading style sheets.css html page ma styleing karna
           ka leya use hoti hai.       
 
    
- Introduction of JavaScript
+Introduction of JavaScript
 
           javascript ak high level programming language hai. jisa hum web ma 
           use karta hai. javascript ki help se hum website ko intractive bana
@@ -49,12 +49,30 @@ what is back-end ?
          back-end ma use karna ka leya node.js use ma le jati hai.
   
          
- History of javascript 
-         Brendan Eich in 1995        
+History of javascript 
+
+         Brendan Eich in 1995   
+         javascript ka phala moch name tha bad ma live script huaa or 
+         last ma javascript huaa.kyo ki java language us time popular thi.
 
 
+what is ECMA 
+
+        ECMA stand for European computer manufacture Association.
+
+        ECMA script ak organization hai.jo scripting language jasay ki 
+        javascript va other language jo ki web ka leya use hoti hai un 
+        ka leya standerd banati hai koi bhi scripting language ecma script
+         ka rule follow karti hai.ecma script ko es bhi boltay hai.
 
 
+        virson :
+              1)es1=1997
+              2)es5=2009
+              3)es6=2015
+
+ ES5 or  ES6 ma kafi Gape tha is resion sa ab har sal ecma script ka new 
+ virson ata hai.  
 
                      
 */
